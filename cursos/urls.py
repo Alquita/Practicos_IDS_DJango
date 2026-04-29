78823dfef789cabd966eedd138445f453d516d38
Alquita <v.alcarazsgarlatta@itecriocuarto.org.ar>
2026-04-28 21:47:07 -0300

    se agrego los formularios y las vistas para los cruds correspondientes y se agrego las urls

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='index'),
-    path('cursos/', views.cursos, name='cursos')
+    path('cursos/', views.cursos, name='cursos'),
+    path('cursos/crear/', views.crear_curso, name='crear_curso'),
+    path('cursos/editar/<int:curso_id>/', views.editar_curso, name='editar_curso'),
+    path('cursos/eliminar/<int:curso_id>/', views.eliminar_curso, name='eliminar_curso')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
 from .models import Curso
+from .forms import CursoForm
 
 def inicio(request):
     return render(request, 'cursos/index.html')
@@ -7,3 +8,34 @@ def inicio(request):
 def cursos(request):
     cursos = Curso.objects.all().order_by("-id")
     return render(request, 'cursos/cursos.html', {'cursos': cursos})
+
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'cursos/crear_curso.html', {'form': form})
+
+def editar_curso(request, curso_id):
+    curso = get_list_or_404(Curso, id=curso_id)
+    if request.method == 'POST':
+        form = CursoForm(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect('cursos')
+    else:
+        form = CursoForm(instance=curso)
+    return render(request, 'cursos/editar_curso.html', {'form': form,})
+
+
+def eliminar_curso(request, curso_id):
+    curso = get_list_or_404(Curso, id=curso_id)
+    if request.method == 'POST':
+        curso.activo = False
+        curso.save()
+        
+        return redirect('cursos')
+    return render(request, 'cursos/eliminar_curso.html', {'curso': curso})
