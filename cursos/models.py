d7aaf2d67fa57d30548e67df49b3120c0fab2c3e
Alquita <v.alcarazsgarlatta@itecriocuarto.org.ar>
2026-04-28 21:21:21 -0300

    se agrego un nuevo modelo con foreing key

--- cursos/models.py
+++ cursos/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
+class Modalidad(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    actvo = models.BooleanField(default=True, null=True)
+
+
+    def __str__(self):
+        return self.nombre
+
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     duracion = models.IntegerField()
     precio = models.IntegerField()
     nivel = models.CharField(max_length=100)
-    modalidad = models.CharField(max_length=100)
     activo = models.BooleanField(default=True)
+    modalidad = models.ForeignKey(
+        Modalidad, 
+        on_delete=models.CASCADE, 
+        related_name='cursos',
+        null=True,
+        blank=True,
+        default=None
+        )
 
     def __str__(self):
         return f'Soy el curso {self.nombre}'
