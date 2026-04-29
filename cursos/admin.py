f36ac21b12f0ab004c7e059c08693dcb19bf9e6b
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2026-04-28 21:21:10 -0300

    mejorada vista admin de Curso

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Curso
 # Register your models here.
 
-admin.site.register(Curso)
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'duracion', 'precio', 'nivel', 'modalidad', 'activo')
+    list_filter = ('modalidad', 'activo',)
+    search_fields = ('nombre', )
