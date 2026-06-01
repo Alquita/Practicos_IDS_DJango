c63636abf46c7031130890f14a82861cf769df20
Joaquin-CasVar <j.castellanovarela@itecriocuarto.org.ar>
2026-06-01 17:09:25 -0300

    formulario de registro con campo de imagen

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from usuarios.views import registrarse
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cursos.urls')),
+    path('accounts/register/', registrarse, name='register'),
 ]
 
 if settings.DEBUG:
